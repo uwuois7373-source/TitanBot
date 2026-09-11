34971228776b8a1edb8a336a420499875d6622e9
uwuois7373-source <uwuois7373@gmail.com>
2026-09-11 20:19:27 +0100

    Update help.js

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -101,7 +101,7 @@ export async function createInitialHelpMenu(client) {
             },
             {
                 name: '\u200B',
-                value: `-# ${botName} is [open source](https://youtu.be/1jCZX8s3bJE?si=NPOYx-vxVE1I5vJK)`,
+                value: `-# ${botName} is [open source](https://youtube.com/@cryotx?si=D6gQPcYgmIx03pqd)`,
                 inline: false,
             },
         ],
