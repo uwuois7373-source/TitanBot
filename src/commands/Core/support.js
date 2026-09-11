f4dfabd295a68ea9529ecfe137c2719526b187a3
uwuois7373-source <uwuois7373@gmail.com>
2026-09-11 20:28:12 +0100

    Update support.js

--- src/commands/Core/support.js
+++ src/commands/Core/support.js
@@ -1,18 +1,20 @@
 import { SlashCommandBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, MessageFlags } from 'discord.js';
 import { createEmbed } from '../../utils/embeds.js';
 import { logger } from '../../utils/logger.js';
-
 import { InteractionHelper } from '../../utils/interactionHelper.js';
-const SUPPORT_SERVER_URL = "https://discord.gg/zRaZuJfPp";
+
+// 1. غيّر الرابط هنا ليكون رابط سيرفرك الخاص
+const SUPPORT_SERVER_URL = "https://discord.gg/YOUR_INVITE_LINK"; 
+
 export default {
-    data: new SlashCommandBuilder()
-    .setName("𝐃𝐙  𝐓𝐎𝐏  | 𝐌𝐎𝐃𝐒  5𝐊")
-    .setDescription("Get link to the  𝐃𝐙  𝐓𝐎𝐏  | 𝐌𝐎𝐃𝐒  5𝐊"),
+  data: new SlashCommandBuilder()
+    .setName("support") // 
+    .setDescription("Get link to the official support server"),
 
   async execute(interaction) {
     try {
       const supportButton = new ButtonBuilder()
-        .setLabel("𝐃𝐙  𝐓𝐎𝐏  | 𝐌𝐎𝐃𝐒  5𝐊")
+        .setLabel("𝐃𝐙  𝐓𝐎𝐏  | 𝐌𝐎𝐃𝐒  5𝐊") // النص الظاهر على الزر
         .setStyle(ButtonStyle.Link)
         .setURL(SUPPORT_SERVER_URL);
 
@@ -20,7 +22,10 @@ export default {
 
       await InteractionHelper.safeReply(interaction, {
         embeds: [
-          createEmbed({ title: "Need Help?", description: "Join our official support server for assistance, report bugs, or suggest features. If you are customizing this bot, remember to change the link in the code!" }),
+          createEmbed({ 
+            title: "Need Help?", 
+            description: "Join our official support server for assistance, report bugs, or suggest features." 
+          }),
         ],
         components: [actionRow],
         flags: MessageFlags.Ephemeral,
@@ -37,5 +42,5 @@ export default {
         logger.error('Failed to send error reply:', replyError);
       }
     }
-  },
+  }
 };
